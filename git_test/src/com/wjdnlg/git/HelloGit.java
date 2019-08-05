3096f290031e55faf217eb1ffeb5796530afe932
wjdnlg <wjdnlg@521.com>
2019-08-05 15:01:49 +0800

    把本地工程添加到git

---
+++ git_test/src/com/wjdnlg/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.wjdnlg.git;
+
+/**
+ * @author zjd
+ * @date 2019/8/5-14:46
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("hello git project");
+    }
+    public void test(){
+        int i=0;
+       i=i+1;
+    }
+}
